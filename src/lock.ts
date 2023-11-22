52358c49add2218679ee18bb43cdeb563cc55846
Julien Capellari <julien.capellari@gmail.com>
2023-11-22 23:50:30 +0100

    Make Lock acquire disposable (#245)
    * feat: make lock acquire disposable
    
    * feat: use using in with

--- src/lock.ts
+++ src/lock.ts
@@ -2,31 +2,35 @@ import { Condition } from './condition.js';
 import { Awaitable } from './types.js';
 
 // Class
-export class Lock {
+export class Lock implements Disposable {
   // Attributes
   private _count = 0;
   private readonly _locked = new Condition(() => this._count > 0);
 
   // Methods
-  async acquire(): Promise<void> {
+  async acquire(): Promise<this> {
     await this._locked.waitFor(false);
 
     this._count++;
     this._locked.check();
+
+    return this;
   }
 
   release(): void {
-    this._count--;
+    this._count = Math.max(this._count - 1, 0);
     this._locked.check();
   }
 
   async with<R>(fn: () => Awaitable<R>): Promise<R> {
-    try {
-      await this.acquire();
-      return await fn();
-    } finally {
-      this.release();
-    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    using _ = await this.acquire();
+
+    return fn();
+  }
+
+  [Symbol.dispose](): void {
+    this.release();
   }
 
   // Properties
